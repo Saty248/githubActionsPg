d5a7e40be8212a767fcfaa6b4e2cfe07cadd239f
Saty248 <mondal.sayantan1234@gmail.com>
2025-01-18 21:26:11 +0530

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+//a simple express server
+import express from 'express';
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000');
+});
