9eded02062b23c77d5609145e4e94689f2e9ea02
Saty248 <mondal.sayantan1234@gmail.com>
2025-01-20 13:57:31 +0530

    fix:remove package

--- release.config.js
+++ release.config.js
@@ -5,27 +5,24 @@ module.exports = {
         '@semantic-release/commit-analyzer',
         '@semantic-release/release-notes-generator',
         '@semantic-release/changelog',
-        '@semantic-release/npm',
         '@semantic-release/github',
         '@semantic-release/git'
     ],
     tagFormat: '${version}',
     verifyConditions: [
         '@semantic-release/changelog',
-        '@semantic-release/npm',
         '@semantic-release/github',
         '@semantic-release/git'
     ],
     prepare: [
         '@semantic-release/changelog',
-        '@semantic-release/npm',
+
         {
             path: '@semantic-release/git',
             message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}'
         }
     ],
     publish: [
-        '@semantic-release/npm',
         '@semantic-release/github'
     ],
     success: [
