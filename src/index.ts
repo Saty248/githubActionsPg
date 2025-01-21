04e0db44c5b33509b81413d614cb377845615222
Saty248 <mondal.sayantan1234@gmail.com>
2025-01-22 02:46:48 +0530

    hotfix1

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ app.get('/', (req, res) => {
 
   res.send('Hello World! This is a simple express server');
 
-  console.log('Request received from: 2');
+  console.log('Request received from: 3');
  
 });
 
